c39638e5d4cbad0d8bde0f55cbbbfb7d2d94c6d8
Ulvi <ulvi.shabanov@gmail.com>
2024-05-26 22:42:31 +0400

    completed todo

---
+++ script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const inputAdd = document.querySelector(".inputAdd");
+const btnAdd = document.querySelector(".btnAdd");
+const list = document.querySelector(".list");
+
+btnAdd.addEventListener("click", (e) => {
+  e.preventDefault();
+  list.innerHTML += `
+        <li class="list__item">
+          <input
+            type="text"
+            class="list__input"
+            value="${inputAdd.value}"
+            disabled
+            autofocus
+            size="${inputAdd.value.length}"
+          />
+          <div class="btn-edit" data-set="edit">
+            <i class="fa-solid fa-pen-to-square edit" data-set="edit"></i>
+          </div>
+          <div class="btn-check" data-set="check">
+            <i class="fa-solid fa-check check" data-set="check"></i>
+          </div>
+          <div class="btn-delete" data-set="delete">
+            <i class="fa-solid fa-trash delete" data-set="delete"></i>
+          </div>
+        </li>
+  `;
+
+  inputAdd.value = "";
+});
+
+function myFunc(e) {
+  if (e.target.closest(".btn-edit")) {
+    e.target
+      .closest(".btn-edit")
+      .parentElement.querySelector(".list__input")
+      .removeAttribute("disabled");
+    e.target
+      .closest(".btn-edit")
+      .parentElement.querySelector(".list__input")
+      .focus();
+
+    document.querySelectorAll(".list__input").forEach((item) => {
+      item.addEventListener("focusout", function () {
+        if (this.value !== "" && confirm("Are you sure?") === true) {
+          this.setAttribute("disabled", "");
+          this.setAttribute("value", `${this.value}`);
+        }
+      });
+    });
+  }
+
+  if (e.target.closest(".btn-check")) {
+    e.target
+      .closest(".btn-check")
+      .parentElement.querySelector(".list__input").style.color =
+      "rgba(202, 179, 153, 0.5)";
+    e.target
+      .closest(".btn-check")
+      .parentElement.querySelector(".list__input").style.textDecoration =
+      "line-through";
+    e.target.closest(".btn-check").parentElement.style.transform =
+      "scale(0.95)";
+  }
+
+  if (e.target.closest(".btn-delete")) {
+    e.target.closest(".btn-delete").parentElement.remove();
+  }
+}
+
+list.addEventListener("click", myFunc);
